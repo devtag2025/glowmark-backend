acd5185629c084a3db269240d3a81fdca462bbb5
faiqaN27 <faiqanadeem6725@gmail.com>
2026-02-16 17:39:30 +0500

    Project Setup

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import { express } from "express";
-import { dotenv } from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
-
 const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.json("Api is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on PORT`, PORT);
 });
